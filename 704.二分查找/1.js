cd541667e72db8501f3eb1fb474c921ffdf215dd
shuyan0723 <2737559680@qq.com>
2025-08-03 20:50:21 +0800

    704二分查找

---
+++ 704.二分查找/1.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let middle = left + ((right - left) >> 1);
+  while (left <= right) {
+
+    if (nums[middle] > target) {
+      right = middle - 1;
+    } else if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      return middle;
+    }
+
+  }
+  return -1;
+};
