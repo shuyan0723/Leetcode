85bea3a86473a31fe2896e741e2b77308f0358e3
shuyan0723 <2737559680@qq.com>
2025-10-06 10:18:32 +0800

    选择排序

--- 排序算法/选择排序/1.js
+++ 排序算法/选择排序/1.js
@@ -1,20 +1,26 @@
-function selecttionSort(arr){
-    const {length} = arr;
+function selectionSort(arr){
+   const {length} =arr
 
-    let indexMin
-    for(let i=0; i<length-1; i++){
-       indexMin = i;
-       for(let j=i; j<length; j++){
-        if(arr[j] < arr[indexMin]){
-            indexMin = j;
-        }
-       }
-       
-       if(indexMin !== i){
-        [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]]
-       }
-
-    }
+   let indexMin
+   for(let i=0;i<length-1;i++){
+    indexMin=i;
+   
+   for(let j=i;j<length;j++){
+     if(arr[indexMin]>arr[j]){
+        indexMin=j;
+     }
+   }
+   if(i!==indexMin){
+      swap(arr,i,indexMin)
+   }
+ }
+ console.log(arr);
+ 
 }
 
-selecttionSort([5,4,3,2,1])
+
+function swap(arr,a,b){
+    [arr[a],arr[b]]=[arr[b],arr[a]];
+}
+
+selectionSort([1,4,3,2,5])
